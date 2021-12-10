dbca49ea2cd1e1b36b261e49786ddf2dae84e4c3
Sreemoyee Basu <crystalwills@Sreemoyees-MacBook-Air.local>
2021-12-10 09:45:48 +0530

    Setup backend folders

---
+++ server/routes/posts.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createPosts, getPosts } from "../controllers/posts.js";
+
+const router = express.Router();
+//localhost:5000/posts
+router.get("/", getPosts);
+router.post("/", createPosts);
+
+export default router;
